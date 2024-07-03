6da7c6fc69912b6f3885ac93e8f224df30b80708
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2024-07-03 11:46:39 +0500

    Testing + Fixing ....

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const data = []
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["https://connecting-react-with-backend.vercel.app"],
+        origin: ["http://localhost:5173/"],
         methods: ["POST", "GET"],
         credentials: true
     }
